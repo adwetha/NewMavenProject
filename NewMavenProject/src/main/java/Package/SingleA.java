43f8fec251c757f468bf3053b656a9360ff85622
adwetha <167293244+adwetha@users.noreply.github.com>
2024-06-27 10:23:28 +0530

    Update SingleA.java

--- NewMavenProject/src/main/java/Package/SingleA.java
+++ NewMavenProject/src/main/java/Package/SingleA.java
@@ -1,5 +1,12 @@
 package Package;
 
 public class SingleA {
+int a=20;
+public void run() {
+	System.out.println("parent class");
+}
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
 
+	}
 }
